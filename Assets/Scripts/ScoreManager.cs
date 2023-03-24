542dd968d220d2f8a85db2b99d6d593423d3a72f
vlrvdnkk <krasilnikovaaa21@st.ithub.ru>
2023-03-24 22:10:58 +0300

    main menu prototype added

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MatchManager
+{
+    [SerializeField] private int _winScore;
+    [SerializeField] private TMP_Text _player0ScoreText;
+    [SerializeField] private TMP_Text _player1ScoreText;
+
+
+    private int _scorePlaye0;
+    private int _scorePlaye1;
+
+    private void Start()
+    {
+        _scorePlaye0 = 0;
+        _scorePlaye1 = 0;
+    }
+
+    public void Score(int player)
+    {
+        if (player == 0)
+            _scorePlaye1++;
+        else if(player == 1)
+            _scorePlaye0++;
+
+        _player0ScoreText.text = _scorePlaye0.ToString();
+        _player1ScoreText.text = _scorePlaye1.ToString();
+
+        if (_scorePlaye0 >= _winScore)
+            PlayerWin(0);
+        else if (_scorePlaye1 >= _winScore)
+            PlayerWin(1);
+
+        ReloadMatch(player);
+    }
+
+    private void PlayerWin(int player)
+    {
+        Debug.Log("Player " + (player+1) + " wins");
+    }
+}
